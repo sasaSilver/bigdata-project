0c635835d9c12c0e807a49bacdd55a47ff547c58
sasaSilver <al.mikhailov@innopolis.university>
2026-05-04 02:29:42 +0300

    fix: deps

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -4,12 +4,13 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # install dependencies
+echo "Installing dependencies..."
 uv sync
 
 # load environment variables
 export $(grep -v '^#' .env | xargs)
 
-
+echo "Dropping existing tables..."
 
 # drop existing tables
 .venv/bin/alembic downgrade base
