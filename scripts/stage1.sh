679d10b0741bb1d200f051094ae0c995648e725e
sasaSilver <al.mikhailov@innopolis.university>
2026-05-04 04:13:49 +0300

    fix: use gdown from venv

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -4,7 +4,7 @@ echo "[Stage 1] Creating database schema..."
 .venv/bin/alembic upgrade head
 
 echo "[Stage 1] Downloading the dataset..."
-gdown "1_TRU4n9Jcs-fSxSYb5tEbFcR9rfLO7rJ" -O data/chess_moves_dataset.csv
+.venv/bin/gdown "1_TRU4n9Jcs-fSxSYb5tEbFcR9rfLO7rJ" -O data/chess_moves_dataset.csv
 
 echo "[Stage 1] Ingesting data into postgres..."
 uv run python -m scripts.insert_data
