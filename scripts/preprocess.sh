128436cd439b862cb9e45b01609a3b3bc36cdefa
sasaSilver <al.mikhailov@innopolis.university>
2026-05-04 05:21:22 +0300

    feat: add hdfs cleanup in preprocess

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source scripts/load_env.sh
+
 # install dependencies
 echo "[Preprocess] Installing dependencies..."
 # install uv
@@ -7,5 +9,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 uv sync
 
 echo "[Preprocess] Dropping existing tables..."
-# drop existing tables
 .venv/bin/alembic downgrade base
+
+echo "[Preprocess] Cleaning up HDFS warehouse..."
+hdfs dfs -rm -r -skipTrash hdfs://${HDFS__WAREHOUSE_URI}:${HDFS__WAREHOUSE_PORT}/user/${PG__USER}/project/warehouse
